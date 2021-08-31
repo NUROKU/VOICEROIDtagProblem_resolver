f031e990e05173ad0cc65aeec642aa83558eeee9
AyumuNekozuki <ayumu@nekozuki.me>
2021-08-31 15:38:20 +0900

    複数できるようにArrayをtableにした

--- content.js
+++ content.js
@@ -1,11 +1,18 @@
 
 /* --- 前方一致で検知させる単語・ORで付け加える単語を定義(あらかじめ小文字で定義) --- */
-const TAG_WORD_INDEXOF_ARRAY = ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク"];
-const TAG_WORD_ADDWORD_ARRAY = ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク", "voiceroid", "ボイスロイド", "ボイロ", "cevio", "cevio_ai", "a.i.voice", "voicebox", "coefont"];
+const TAG_WORD_INDEXOF_TABLE = [
+    ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク"],
+    ["ソフトウェアシンガー", "vocaloid"]
+];
+const TAG_WORD_ADDWORD_TABLE = [
+    ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク", "voiceroid", "ボイスロイド", "ボイロ", "cevio", "cevio_ai", "a.i.voice", "voicebox"],
+    ["ソフトウェアシンガー", "vocaloid", "ボカロ", "utau", "cevio", "synthv", "synthesizerv", "neutrino", "歌うボイスロイド"]
+];
+
 
 /* カウンターとか */
 let is_match = false; //前方一致するかどうか
-let try_counter = 0; //ORがいくつあるか）
+let table_col; //前方一致したときのテーブル列
 
 //URL取得してタグの部分だけ良い感じに抜き出す
 // "/tag/ソフトウェアトーク" みたいな感じに取得されるから、/tag/を空白に置換している
@@ -14,28 +21,39 @@ var tag = decodeURI(url_path.replace("/tag/", ""));
 
 //ORの数を数える
 var ORcount = tag.match(/OR/g);
-if(ORcount){
-    try_counter = ORcount.length;
-}
 
 /* --- 前方一致のテーブルを舐め回す --- */
-for (var n = 0; n < TAG_WORD_INDEXOF_ARRAY.length; n++) {
-    var tagname = TAG_WORD_INDEXOF_ARRAY[n];
+for (var n = 0; n < TAG_WORD_INDEXOF_TABLE.length; n++) {
+    //とりあえず配列を代入
+    var tagname_arr = TAG_WORD_INDEXOF_TABLE[n];
+
+    //タグを小文字化
+    tag = tag.toLowerCase();
+
+    //前方一致するものを探す
+    var match_tagname = tagname_arr.find(el => tag.startsWith(el));
+    console.log(match_tagname);
+    console.log(tag)
+
     /* --- 前方一致したら --- */
-    if (tag.startsWith(tagname)) {
+    if (match_tagname) {
         is_match = true;
         /* --- 前方一致箇所を抜いた文字列を取得 ex:ソフトウェアトーク劇場 → 劇場 --- */
-        var genre = tag.replace(tagname, "");
+        var genre = tag.replace(match_tagname, "");
+        table_col = n;
     }
 }
 
 //undefinedが文字列として出力されないようにする
 if (genre == undefined) genre = "";
-var url_path = TAG_WORD_ADDWORD_ARRAY.join(genre + '+OR+') + genre;
+//ORが無限に出ないようにする
+if (genre.match(/OR/g)) genre = genre.substr(0, genre.indexOf("+OR"));
 
-//前方一致し、ORが7個以下のときに実行
-//ORの上限をつけないと無限に実行してしまうため
-if (is_match && (try_counter < 7)) {
+//ジャンルを追記しながらURLにする
+url_path = TAG_WORD_ADDWORD_TABLE[table_col].join(genre + '+OR+') + genre;
+
+//前方一致し、入力文字列にORが無い時に実行
+if (is_match && (!ORcount)) {
     var url = "https://www.nicovideo.jp/tag/" + encodeURI(url_path)
     location.href = url
 }
--- webRequestAPI/background.js
+++ webRequestAPI/background.js
@@ -3,12 +3,18 @@
 if (typeof browser === 'undefined') browser = chrome;
 
 /* --- 前方一致で検知させる単語・ORで付け加える単語を定義(あらかじめ小文字で定義) --- */
-const TAG_WORD_INDEXOF_ARRAY = ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク"];
-const TAG_WORD_ADDWORD_ARRAY = ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク", "voiceroid", "ボイスロイド", "ボイロ", "cevio", "cevio_ai", "a.i.voice", "voicebox"];
+const TAG_WORD_INDEXOF_TABLE = [
+    ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク"],
+    ["ソフトウェアシンガー", "vocaloid"]
+];
+const TAG_WORD_ADDWORD_TABLE = [
+    ["ソフトウェアトーク", "ボイチェビ", "ボイチェビトーク", "voiceroid", "ボイスロイド", "ボイロ", "cevio", "cevio_ai", "a.i.voice", "voicebox"],
+    ["ソフトウェアシンガー", "vocaloid", "ボカロ", "utau", "cevio", "synthv", "synthesizerv", "neutrino", "歌うボイスロイド"]
+];
 
 /* カウンターとか */
 let is_match = false; //前方一致するかどうか
-let try_counter = 0; //ORがいくつあるか）
+let table_col; //前方一致したときのテーブル列
 let url_path; //複数回動いてほしくないので事前定義
 
 /* --- リクエストが送信される前にリダイレクトをかける --- */
@@ -19,31 +25,40 @@ browser.webRequest.onBeforeRequest.addListener(details => {
 
     //ORの数を数える
     var ORcount = tag.match(/OR/g);
-    if (ORcount) {
-        try_counter = ORcount.length;
-    }
 
     /* --- 前方一致のテーブルを舐め回す --- */
-    for (var n = 0; n < TAG_WORD_INDEXOF_ARRAY.length; n++) {
-        var tagname = TAG_WORD_INDEXOF_ARRAY[n];
+    for (var n = 0; n < TAG_WORD_INDEXOF_TABLE.length; n++) {
+        //とりあえず配列を代入
+        var tagname_arr = TAG_WORD_INDEXOF_TABLE[n];
+
+        //タグを小文字化
+        tag = tag.toLowerCase();
+
+        //前方一致するものを探す
+        var match_tagname = tagname_arr.find(el => tag.startsWith(el));
+        console.log(match_tagname);
+        console.log(tag)
+
         /* --- 前方一致したら --- */
-        if (tag.startsWith(tagname)) {
+        if (match_tagname) {
             is_match = true;
             /* --- 前方一致箇所を抜いた文字列を取得 ex:ソフトウェアトーク劇場 → 劇場 --- */
-            var genre = tag.replace(tagname, "");
+            var genre = tag.replace(match_tagname, "");
+            table_col = n;
         }
     }
 
-    //undefinedが文字列として出力されないようにする
-    if (genre == undefined) genre = "";
-    //ORが無限に出ないようにする
-    if (genre.match(/OR/g)) genre = genre.substr(0, genre.indexOf("+OR"));
+     //前方一致・ORが5個以下なら実行 
+    if (is_match && (!ORcount)) {
+        //undefinedが文字列として出力されないようにする
+        if (genre == undefined) genre = "";
+        //ORが無限に出ないようにする
+        if (genre.match(/OR/g)) genre = genre.substr(0, genre.indexOf("+OR"));
 
-    //ジャンルを追記しながらURLにする
-    url_path = TAG_WORD_ADDWORD_ARRAY.join(genre + '+OR+') + genre;
+        //ジャンルを追記しながらURLにする
+        url_path = TAG_WORD_ADDWORD_TABLE[table_col].join(genre + '+OR+') + genre;
 
-    //前方一致・ORが5個以下なら実行 
-    if (is_match && ORcount < 5) {
+        //リダイレクト
         url.pathname = '/tag/' + url_path;
         return { redirectUrl: url.href };
     }
